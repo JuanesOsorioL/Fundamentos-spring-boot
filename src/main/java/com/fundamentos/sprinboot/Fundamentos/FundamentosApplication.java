00e96e4675f05e64db03c26547183bc2c7339bf1
juanesosorio <Juanchis_1989@hotmail.com>
2021-06-12 23:05:03 -0500

    first commit

---
+++ src/main/java/com/fundamentos/sprinboot/Fundamentos/FundamentosApplication.java
@@ -0,0 +1,14 @@
+package com.fundamentos.sprinboot.Fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
